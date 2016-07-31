d2290121843f11c5d16affb3c93c4d67a8a09842
tadashiy1012 <tadashi.y1012@gmail.com>
2016-07-31 14:52:35 +0900

    1st commit

---
+++ gtk-shape_rensyu/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+
+	private int count;
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		this.count = 0;
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton5Clicked (object sender, EventArgs e)
+	{
+		this.count += 1;
+		this.label2.Text = this.count.ToString();
+	}
+}
